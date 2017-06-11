482c8f15d4e633b5317374f07ca56a728d603613
Roger Silva <rogersilva84@gmail.com>
2017-06-11 09:47:30 -0300

    Initial commit

---
+++ WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Diminuir_Click(object sender, RoutedEventArgs e)
+        {
+            this.TamanhoFonte.Value -= 1;
+        }
+
+        private void Aumentar_Click(object sender, RoutedEventArgs e)
+        {
+            this.TamanhoFonte.Value += 1;
+        }
+    }
+}
